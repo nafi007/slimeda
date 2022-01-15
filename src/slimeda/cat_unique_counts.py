d71d7868e9bffe92917e88dad9b95b0d18ce9183
thayehas <thayehas@gmail.com>
2022-01-15 13:27:56 -0800

    merge branch with main

---
+++ src/slimeda/cat_unique_counts.py
@@ -0,0 +1,15 @@
+def cat_unique_counts(df):
+    """
+    Return unique value count of categorical features
+    Parameters
+    ----------
+    df: pd.DataFrame
+        pandas dataframe
+    Returns
+    -------
+    pd.DataFrame
+    Examples
+    --------
+    >>>from slimeda import cat_unique_counts
+    >>>cat_unique_counts(df)
+    """
---
+++ src/slimeda/miss_counts.py
@@ -0,0 +1,16 @@
+def miss_counts(df):
+    """
+    Return the number of missing values for each column in the df
+    Parameters
+    ----------
+    df: pd.DataFrame
+        pandas dataframe
+    Returns
+    -------
+    pd.DataFrame
+    Examples
+    --------
+    >>>from slimeda import miss_counts
+    >>>miss_counts(df)
+    """
+    
