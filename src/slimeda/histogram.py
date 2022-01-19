a33293627f0bf98b2e7c1006fe8197b5fa25f954
khalidcawl <khalidca@student.ubc.ca>
2022-01-18 20:23:25 -0800

    More tests added

--- src/slimeda/histogram.py
+++ src/slimeda/histogram.py
@@ -1,4 +1,5 @@
 import altair as alt
+import pandas as pd
 
 def histogram(df, columns, path=""):
     """
@@ -17,6 +18,19 @@ def histogram(df, columns, path=""):
     >>>from slimeda import histogram
     >>>histogram(df, ['age', 'income'])
     """
+
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("'df' should be of type pd.DataFrame")
+
+    if not isinstance(columns, list):
+        raise TypeError("'columns' should be of type list")
+
+    if len(columns) == 0:
+        raise ValueError("There should be at least column in the dataframe 'df' provided")
+
+    for col in columns:
+        if col not in df.columns:
+            raise ValueError(f"The column {col} is not in the dataframe provided 'df'") 
 
     charts = []
     for col in columns:
--- tests/test_histogram.py
+++ tests/test_histogram.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 from slimeda.histogram import histogram
+import pytest
 
 def test_histogram_single_chart():
     rng = np.random.default_rng()
 
-    # create a dummy date frame with four columns for testing
+    # create a dummy data frame with four columns for testing
     df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
-    charts = histogram(df, "A")
+    charts = histogram(df, ["A"])
     assert charts != None
     assert len(charts) == 1
     assert charts[0] != None
@@ -17,7 +18,7 @@ def test_histogram_single_chart():
 def test_histogram_multiple_charts():
     rng = np.random.default_rng()
 
-    # create a dummy date frame with four columns for testing
+    # create a dummy data frame with four columns for testing
     df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
     charts = histogram(df, ["A", "B", "C"])
     assert charts != None
@@ -26,3 +27,19 @@ def test_histogram_multiple_charts():
         assert chart != None
         assert chart.data.equals(df)
         assert chart.mark == "bar"
+
+def test_histogram_invalid_dataframe():
+    with pytest.raises(TypeError):
+        histogram([1,2,3,4])
+
+def test_histogram_no_columns():
+    with pytest.raises(ValueError):
+        rng = np.random.default_rng()
+        df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
+        histogram(df, [])
+
+def test_histogram_wrong_column():
+    with pytest.raises(ValueError):
+        rng = np.random.default_rng()
+        df = pd.DataFrame(rng.integers(0, 100, size=(100, 4)), columns=list('ABCD'))
+        histogram(df, ["E"])
